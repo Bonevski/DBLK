b6557f765f040f2f124bbfa0ae58e36eda5e9eb2
Christoph Knips <95695977+Ratzz91@users.noreply.github.com>
2022-06-08 15:25:24 +0200

    IBookingDB verändert

--- src/de/fhswf/DBLK/DataManagement/IBookingDB.java
+++ src/de/fhswf/DBLK/DataManagement/IBookingDB.java
@@ -1,4 +1,11 @@
-package DataManagement;
+package de.fhswf.DBLK.DataManagement;
+
+import java.util.Date;
+
 
 public interface IBookingDB {
+
+static void setBooking(Date bookDate,String bookRoom, String bookUser){
+
+}
 }
