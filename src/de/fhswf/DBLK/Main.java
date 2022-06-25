324e49a5fe737d9a010579b6de7d1ed6c4854956
Sasha Bonevski <Bonevski.sasha@fh-swf.de>
2022-06-25 12:15:17 +0200

    Menü erweitert Login hinzugefügt

--- src/de/fhswf/DBLK/Main.java
+++ src/de/fhswf/DBLK/Main.java
@@ -1,8 +1,26 @@
 package de.fhswf.DBLK;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /**
+         * Hier wird der Login versucht
+         */
+        JTextField username = new JTextField();
+        JTextField password = new JPasswordField();
+        Object[] message = {"Username:", username, "Password:", password};
+
+        int option = JOptionPane.showConfirmDialog(null, message, "Login", JOptionPane.OK_CANCEL_OPTION);
+        if (option == JOptionPane.OK_OPTION) {
+            if (username.getText().equals("Bonevski") && password.getText().equals("Test")) {
+                System.out.println("Login successful");
+            } else {
+                System.out.println("login failed");
+            }
+        } else {
+            System.out.println("Login canceled");
+        }
     }
 }
--- src/de/fhswf/DBLK/UI/UI.java
+++ src/de/fhswf/DBLK/UI/UI.java
@@ -1,31 +1,72 @@
 package de.fhswf.DBLK.UI;
+
 import java.util.Scanner;
 
+/**
+ * @author Sasha
+ */
 public class UI {
 
-       public static void main(String[] args){
+    public static void main(String[] args) {
 
+        /**
+         * Hier wird das Menü ausgwehählt
+         */
+        System.out.println("Bitte geben Sie Ihre Rolle ein: ");
+        System.out.println("'1' --> Student | '2' --> Admin");
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hallo, bitte wähle ein Punkt aus:");
-        System.out.println("1 für Raumanzeige");
-        System.out.println("2 für Raumbuchung");
-        System.out.println("3 für Raumstornierung");
+        int role = Integer.parseInt(sc.nextLine());
 
-        int var = Integer.parseInt(sc.nextLine());
-
-        switch (var) {
+        switch (role) {
             case 1:
-                System.out.println("Raumanzeige:");
+                System.out.println("Rolle: Student");
+                System.out.println("Bitte wähle ein Punkt aus:");
+                System.out.println("1 für Raumanzeige");
+                System.out.println("2 für Raumbuchung");
+                System.out.println("3 für Raumstornierung");
+                int varStudent = Integer.parseInt(sc.nextLine());
+
+                switch (varStudent) {
+                    case 1:
+                        System.out.println("Raumanzeige:");
+                        // Methode Raumanzeigen aufrufen
+                        break;
+                    case 2:
+                        System.out.println("Raumbuchung:");
+                        // Methode Raumbuchen aufrufen
+                        break;
+                    case 3:
+                        System.out.println("Raumstornierung:");
+                        // Methode Raumstornierung aufrufen
+                        break;
+                    default:
+                        System.out.println("Keine gültige Auswahl (1, 2, 3) getroffen.");
+                }
                 break;
+
             case 2:
-                System.out.println("Raumbuchung:");
-                break;
-            case 3:
-                System.out.println("Raumstornierung:");
+                System.out.println("Rolle: Admin");
+                System.out.println("Bitte wähle ein Punkt aus: ");
+                System.out.println("1 für addUser");
+                System.out.println("2 für removeUser");
+                int varAdmin = Integer.parseInt(sc.nextLine());
+                switch (varAdmin) {
+                    case 1:
+                        System.out.println("addUser wird ausgeführt:");
+                        // Methode addUser aufrufen
+                        break;
+                    case 2:
+                        System.out.println("removeUser wird ausgeführt:");
+                        // Methode removeUser aufrufen
+                        break;
+                    default:
+                        System.out.println("Keine gültige Auswahl (1, 2) getroffen.");
+                }
                 break;
             default:
-                System.out.println("Keine gueltige Auswahl (1, 2, 3) getroffen.");
+                System.out.println("Keine gültige Auswahl (1, 2) getroffen.");
         }
+
     }
 }
