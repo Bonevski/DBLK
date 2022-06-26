cc0a9cb256f479cabcdd0d34c9ed8b7e688ec0c5
Christoph Knips <95695977+Ratzz91@users.noreply.github.com>
2022-06-26 12:17:51 +0200

    Room update

--- src/de/fhswf/DBLK/DataManagement/IRoomPersistence.java
+++ src/de/fhswf/DBLK/DataManagement/IRoomPersistence.java
@@ -1,13 +1,19 @@
 package de.fhswf.DBLK.DataManagement;
 
+import java.util.ArrayList;
+
 public interface IRoomPersistence {
 
 
 
-
+//Methods
 public void deleteRoom (String roomName);
+
+/* not needed:
 public RoomDB getRoom (String roomName);
 public void setRoom (String roomName);
+ */
 
+public  ArrayList getRoomList();
 
 }
--- src/de/fhswf/DBLK/DataManagement/RoomDB.java
+++ src/de/fhswf/DBLK/DataManagement/RoomDB.java
@@ -1,20 +1,32 @@
 package de.fhswf.DBLK.DataManagement;
 
+/**
+ * @author Christoph
+ */
+
+import java.util.ArrayList;
+
 public class RoomDB implements IRoomPersistence{
 
     private String roomName;
-    protected String[] rooms;
+    // protected String[] rooms;  // not needed --> ArrayList
+
+
+    //create list of rooms
+    ArrayList<String> roomList = new ArrayList<String>();
 
     public RoomDB (String roomName){
         this.roomName=roomName;
+        roomList.add(roomName);   // When a new room is created add it to the roomList
+        // more Room information like seats possible
     }
 
 
     @Override
     public void deleteRoom(String roomName) {
-
+  // delete object? overwrite with null und run System.gc() <-- not recommended and does leave open space in  ArrayList
     }
-
+/* not needed
     @Override
     public RoomDB getRoom(String roomName) {
         return null;
@@ -22,6 +34,15 @@ public class RoomDB implements IRoomPersistence{
 
     @Override
     public void setRoom(String roomName) {
+    }
+*/
 
+
+
+    /**
+     * access to roomList from interface
+     */
+    public  ArrayList getRoomList(){
+    return roomList;
     }
 }
