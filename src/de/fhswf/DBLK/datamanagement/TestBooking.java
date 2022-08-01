cf13305d0c720131d155ec619a93cf4623b4e43d
Firat323 <Firat.demir43@yahoo.com>
2022-08-01 17:10:32 +0200

    hi

--- src/de/fhswf/DBLK/datamanagement/TestBooking.java
+++ src/de/fhswf/DBLK/datamanagement/TestBooking.java
@@ -1,9 +1,8 @@
 package de.fhswf.DBLK.datamanagement;
 import de.fhswf.DBLK.datamanagement.RoomDB;
-
-import org.junit.Assertions;
-import org.Junit.jupiter.api.Test;
-import org.junit.jupiter.api;
+import de.fhswf.DBLK.datamanagement.Booking;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * @author Firat
  */
@@ -43,6 +42,7 @@ public class BuchungTest  {
         }//end addBooking()
  */
     //Test vorgang
+    @Test
     public void testBuchung() {
         String ergebnis;
         Room testRoom = new Room("H105");
@@ -52,27 +52,26 @@ public class BuchungTest  {
 
         testDB.addRoom(testRoom);
 
-        assertEquals(testRoom, RoomDB.getRoomList()); //Bei positiver Eingabe, Anzeigen der Raumliste
-        /*
-        assertEquals(0, testUser.getBookingList).size()); //Welche Räume kann man Buchen
+        assertEquals(testRoom, RoomDB.getRoomList()); //Anzeigen der Raumliste
+        assertEquals(0, testRoom.getBookingID).size()); //Welche Räume kann man Buchen
         String ergebnis = testUser.(testRoom);
         assertEquals(false, testRoom.getVerfuegbarkeit()); //nicht Gebuchte Räume anzeigen(Verfügbar)
         assertEquals("Sie haben den Raum H105 erfolgreich gebucht.", ergebnis);
-        assertEquals(1, testUser.getBookingList().size());//Einfügen des Benutzers bei erfolgreicher Buchung
-        */
+        assertEquals(1, testUser.getBookinglist.size());//Einfügen des Benutzers bei erfolgreicher Buchung
+
     }
 
-
-   /* public void testStornieren() {
+    @Test
+    public void testStornieren() {
         Room testRoom = new Room("H105");
         User testUser = new User("Sasha","sb123","Bonevski.sasha@fh-swf.de";
-        testUser.bucheRaum(testRoom); //Anzeigen der gebuchten Räume des Nutzers
+        BookingDB.bookingRoom(testRoom); //Anzeigen der gebuchten Räume des Nutzers
         String ergebnis = testUser.cancelRoom(testRoom);//entfernen der Buchung des Nutzers
         assertEquals("Sie haben die Buchung des Raumes " + testRoom.getName() + " erfolgreich storniert.", ergebnis); //Buchung wurde Storniert.
         assertEquals(0, testUser.getBooking().size());
         assertEquals(true, testRoom.getBookingList());
     }
-    */
+
 
 }
 
